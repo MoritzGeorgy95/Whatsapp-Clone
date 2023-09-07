534cf80ee1a5027e31fbf407cc05b268c65a90a6
Moritz Georgy <moritzgeorgy@MacBook-Pro-von-Moritz.local>
2023-09-07 14:31:43 +0200

    deleted wrong folder

--- src/firebaseConfig.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBdc70PuqgoayW02HtndC8Xjairj2ZMXzA",
-    authDomain: "whatsapp-clone-7803f.firebaseapp.com",
-    projectId: "whatsapp-clone-7803f",
-    storageBucket: "whatsapp-clone-7803f.appspot.com",
-    messagingSenderId: "400964355338",
-    appId: "1:400964355338:web:dafd82fd7e79828224e6f3"
-  };
