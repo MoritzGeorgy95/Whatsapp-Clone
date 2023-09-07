4afd633183bcc0ecb55d7a9774de9a8a3e97d575
Moritz Georgy <moritzgeorgy@MacBook-Pro-von-Moritz.local>
2023-09-07 14:34:57 +0200

    added source folder

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBdc70PuqgoayW02HtndC8Xjairj2ZMXzA",
+    authDomain: "whatsapp-clone-7803f.firebaseapp.com",
+    projectId: "whatsapp-clone-7803f",
+    storageBucket: "whatsapp-clone-7803f.appspot.com",
+    messagingSenderId: "400964355338",
+    appId: "1:400964355338:web:dafd82fd7e79828224e6f3"
+  };
